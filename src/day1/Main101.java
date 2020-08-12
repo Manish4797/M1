14e929d466c98044f92f2cac3e228590eb1129cd
Urgyen Shakya <urgyenshakya@gmail.com>
2020-08-12 08:34:37 +0545

    111

---
+++ src/day1/Main101.java
@@ -0,0 +1,111 @@
+package day1;
+
+public class Main101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// variable
+//
+//		String age = "Twenty Two";
+//		String name = "Apple";
+//		int houseNo = 75555;
+//
+//		double height = 5.9;
+//
+//		int num1 = 10;
+//		int num2 = 25;
+//
+//		double sum = num1 + num2;
+//
+//		System.out.println(sum);
+//
+//		// Naming Convention in Java
+//		// small letter
+//		// _name = $name;
+//		// lower camelCAsing
+//
+//		int rollNumber;
+//		String nameOfTheSudentWhoHasFailedInTest;
+//
+//		int x = 10;
+//		int y = 15;
+//
+//		// data type
+//
+//		// int
+//		// double
+//		// String combination of characters a, b 1 01
+//
+//		char letter = 'a';
+//		String name1 = "name";
+//		
+//		boolean isTrue = false; //kamm sapace? 0 or 1;
+//		//size 1 bit;
+//		
+		// Size of datatypes?
+
+		// operators
+		// 1. unary operators;
+
+//		int num = 10;
+//		num--;
+//		num++;
+//		// num=num+1;
+//		// num=num-1;
+//		System.out.println(num);
+
+		// Arithemtic operators;
+		// + - * /
+		// % modulus
+		int num1 = 10;
+		int num2 = 27;
+
+		int result = num2 % num1;
+		System.out.println(result);
+
+		// Scenarioin
+
+		int noToCheck = 55;
+		if (56 % 2 == 0) {
+			System.out.println("Even");
+		}
+
+		// Logical Operators
+
+		// AND &&
+		// t and f --> f
+//(euta condition )  and  (arko condition)  --> false
+
+		// OR ||
+		// t or f --> t
+//(euta condition )  or  (arko condition)  --> false		
+		// Link //Logic Gates???
+
+//		if(1>3 || 1>3 ) {
+//			System.out.println("Condition true");
+//		}
+
+		// Assignment opertor?
+
+		int age = 10;
+
+		// shortcut approach
+		age += 5;
+		age = age + 5;
+
+		System.out.println(age);
+
+		// Relational
+
+		//ok
+		// 5 > 4
+
+		// > , <
+
+		// Ternary operator?
+		// REsearch about this operator
+
+	}
+
+}
