3ba3a0772128e0c5c36fe1f9592648290a9fbdad
Urgyen Shakya <urgyenshakya@gmail.com>
2020-08-13 08:24:06 +0545

    ok

---
+++ src/day2/Main101.java
@@ -0,0 +1,113 @@
+package day2;
+
+public class Main101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Java
+		// 1. We do something?
+		// Statements?
+
+		// 2. We do something if this or that happens?
+		// if else , decision control statements
+		// if condiiton
+
+//		int age= 15;
+//		
+//		if(age>=18) {
+//			//execute hunxa
+//			System.out.println("You are eligible to vote");
+//		}
+//		else {
+//			System.out.println("You are not eligible to vote");
+//		}
+
+//		Age= 	0-19 Child
+//				20-30 Adult
+//				31-50 Old
+		// 50 + very old
+
+		// Scenario age 0 - 100
+//		int age= 25;
+//		//else if
+//		if(age<20) {
+//			System.out.println("Child");
+//		}
+//		else if(age<=30) {
+//			System.out.println("Adult");
+//		}
+//		else if(age<=50) {
+//			System.out.println("Old");
+//		}
+//		else {
+//			System.out.println("Very Old");
+//		}
+//		
+//		//end
+//		// switch
+//		// 1-7
+//		int weekday = 14;
+//		switch (weekday) {
+//
+//		case 1:
+//			System.out.println("Sunday");
+//			break;
+//
+//		case 2:
+//			System.out.println("Monday");
+//			break;
+//
+//		case 3:
+//			System.out.println("Tuessday");
+//			break;
+//
+//		case 4:
+//			System.out.println("Wed");
+//			break;
+//			
+//		case 5:
+//			System.out.println("Thurs");
+//			break;
+//
+//		case 6:
+//			System.out.println("Friday");
+//			break;
+//			
+//		case 7:
+//			System.out.println("SAt");
+//			break;
+//			
+//		default:
+//			System.out.println("Invalid");
+//
+//		}
+
+		// 3. We do something again and again'
+		// Loops
+		
+		//index initialization ,,  condition; ,, increment or decrement
+		
+		//1. while loop
+	
+		int i=1;
+		
+		while(i <= 5) {
+			System.out.println(i+ " : Urgyen");
+			i++;
+			//10
+		}
+		//end loop
+		
+		
+		
+		//2. do while loop
+		//3. for loop
+		
+		
+		
+		
+
+	}
+
+}
